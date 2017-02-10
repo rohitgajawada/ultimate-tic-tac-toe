c413b96b3b4eb4f7837fdef350ee11b97fe63a7c
rohit gajawada <rohit.gajawada@gmail.com>
2017-02-10 23:15:44 +0530

    made class

--- game.py
+++ game.py
@@ -1,11 +1,18 @@
-def startState():
+# def startState():
 
-def actions(state):
+# def actions(state):
 
-def newState(state, action):
+# def newState(state, action):
 
-def endState(state):
+# def endState(state):
 
-def utility(state):
+# def utility(state):
 
-def player(state):
+# def player(state):
+
+class minimax_agent():
+    def __init__(self):
+        pass
+
+    def move(self, board, old_move, flag):
+        cells = board.find_valid_move_cells(old_move)
