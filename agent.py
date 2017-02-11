85789eb1dbe54380579a48a8c7377036f152e420
rohit gajawada <rohit.gajawada@gmail.com>
2017-02-11 11:48:23 +0530

    put all in one class in minimax.py and deleted simulate.py

--- agent.py
+++
@@ -1,6 +0,0 @@
-class minimax_agent():
-    def __init__(self):
-        pass
-
-    def move(self, board, old_move, flag):
-        cells = board.find_valid_move_cells(old_move)
--- minimax.py
+++ minimax.py
@@ -1,68 +1,73 @@
-def minimaxVal(game, maxdepth, evaluatorFunction = None):
-	if maxdepth == 0 or game.isLeafNode():
-		if evaluatorFunction:
-			return evaluatorFunction(game)
-		else:
-			return game.score()
+class minimax_agent():
 
-def minimax(game, maxdepth, evaluatorFunction = None):
+	def __init__(self):
+    	pass
 
-	best = None
-	for move in game.find_valid_move_cells():
-		temp = game.copy()
-		temp.play(move)
-		val = -1 * minimaxVal(temp, maxdepth, evaluatorFunction)
-		if best is None or val > best[0]:
-			best = (val, move)
+	def minimaxVal(game, maxdepth, evaluatorFunction = None):
+		if maxdepth == 0 or game.isLeafNode():
+			if evaluatorFunction:
+				return evaluatorFunction(game)
+			else:
+				return game.score()
 
-	return best
+	def move(game, maxdepth, evaluatorFunction = None):
 
-def alphabetaVal(game, maxdepth, alpha, beta, evaluatorFunction = None):
-	if maxdepth = 0 or game.isLeafNode():
-		if evaluatorFunction:
-			return evaluatorFunction(game)
-		else:
-			return game.score()
+		best = None
+		for move in game.find_valid_move_cells():
+			temp = game.copy()
+			temp.play(move)
+			val = -1 * minimaxVal(temp, maxdepth, evaluatorFunction)
+			if best is None or val > best[0]:
+				best = (val, move)
 
-	for move in game.find_valid_move_cells():
-		temp = game.copy()
-		temp.play(move)
+		return best
 
-		if alpha is not None:
-			opponentBeta  = -1 * alpha
-		else:
-			opponentBeta = None
+	def alphabetaVal(game, maxdepth, alpha, beta, evaluatorFunction = None):
+		if maxdepth = 0 or game.isLeafNode():
+			if evaluatorFunction:
+				return evaluatorFunction(game)
+			else:
+				return game.score()
 
-		if beta is not None:
-			opponentAlpha = -1 * beta
-		else:
-			opponentAlpha = None
+		for move in game.find_valid_move_cells():
+			temp = game.copy()
+			temp.play(move)
 
-		val = -1 * alphabetaVal(temp, maxdepth - 1, opponentAlpha, opponentBeta, evaluatorFunction)
-		if (alpha is not None) and (beta is not None) and alpha >= beta:
-			return beta
+			if alpha is not None:
+				opponentBeta  = -1 * alpha
+			else:
+				opponentBeta = None
 
-	return alpha
+			if beta is not None:
+				opponentAlpha = -1 * beta
+			else:
+				opponentAlpha = None
 
-def alphabetapruning(game, maxdepth, evaluatorFunction = None):
+			val = -1 * alphabetaVal(temp, maxdepth - 1, opponentAlpha, opponentBeta, evaluatorFunction)
+			if (alpha is not None) and (beta is not None) and alpha >= beta:
+				return beta
 
-	bestVal = None
-	bestMove = None
+		return alpha
 
-	for move in game.find_valid_move_cells():
-		temp = game.copy()
-		temp.play(move)
+	def alphabetapruning(game, maxdepth, evaluatorFunction = None):
 
-		if bestVal is not None:
-			opponentBeta = -1 * bestVal
-		else:
-			opponentBeta = None
+		bestVal = None
+		bestMove = None
 
-		val = -1 * alphabetaVal(temp, maxdepth, None, opponentBeta, evaluatorFunction)
-		if bestVal is None or val > bestVal:
-			(bestVal, bestMove) = (val, move)
+		for move in game.find_valid_move_cells():
+			temp = game.copy()
+			temp.play(move)
 
-	return (bestVal, bestMove)
+			if bestVal is not None:
+				opponentBeta = -1 * bestVal
+			else:
+				opponentBeta = None
+
+			val = -1 * alphabetaVal(temp, maxdepth, None, opponentBeta, evaluatorFunction)
+			if bestVal is None or val > bestVal:
+				(bestVal, bestMove) = (val, move)
+
+		return (bestVal, bestMove)
 
 
 
