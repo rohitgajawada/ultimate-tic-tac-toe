d0596f5d8fb4afc6d70a8403d80717ecf03c6b1c
rohit gajawada <rohit.gajawada@gmail.com>
2017-02-19 21:27:07 +0530

    completed get playable cells and playable blocks and started some fundamental stuff

--- team39.py
+++ team39.py
@@ -18,85 +18,112 @@ win_rows = [
 ]
 
 class Player39():
-"""Bot Class"""
-
+    "AI Bot"
 	def __init__(self):
     	"Constructor"
     	self.board = []
-		self.valid_moves = [];
+		self.valid_moves = []
+		self.blocks_state = [' ' for i in xrange(0, 16)]
+
 		self.flag = " "
 		self.moves = 0
-		self.good_endstate = False
+		self.maxdepth = 4
+		self.good_terminal = False
 
-	def move(self, board, old_move, flag):
+	def is_good_terminal(self, blocks):
+        """Check if a good terminal state is reached."""
+        flag = self.flag
+        if blocks[0] == blocks[1] == blocks[2] == blocks[3] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[4] == blocks[5] == blocks[6] == blocks[7] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[8] == blocks[9] == blocks[10] == blocks[11] == flag:
+            self.good_terminal = True
+            return True
+
+        if blocks[0] == blocks[4] == blocks[8] == blocks[12] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[1] == blocks[5] == blocks[9] == blocks[13] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[2] == blocks[5] == blocks[8] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[2] == blocks[4] == blocks[6] == flag:
+            self.good_terminal = True
+            return True
+        if blocks[0] == blocks[4] == blocks[8] == flag:
+            self.good_terminal = True
+            return True
+
+    def is_bad_terminal(self, blocks):
+    		
+        """Check if a good terminal state is reached."""
+        flag = 'x' if self.flag == 'o' else 'o'
+        if blocks[0] == blocks[1] == blocks[2] == flag:
+            return True
+        if blocks[3] == blocks[4] == blocks[5] == flag:
+            return True
+        if blocks[6] == blocks[2] == blocks[8] == flag:
+            return True
+        if blocks[0] == blocks[3] == blocks[6] == flag:
+            return True
+        if blocks[1] == blocks[4] == blocks[7] == flag:
+            return True
+        if blocks[2] == blocks[5] == blocks[8] == flag:
+            return True
+        if blocks[2] == blocks[4] == blocks[6] == flag:
+            return True
+        if blocks[0] == blocks[4] == blocks[8] == flag:
+            return True
+
+	
+	def move(self, board, old_move, currflag):
     
-		best = None
-		cells = board.find_valid_move_cells(old_move)
+		self.board = board
+		self.flag = currflag
+		self.moves += 1
 
-		for move in cells:
-    			
-			temp = game.copy()
-			temp.play(move)
-			val = -1 * minimaxVal(temp, maxdepth, evaluatorFunction)
-			if best is None or val > best[0]:
-				best = (val, move)
+		bestmove = self.alphabeta(board, blocks_state, old_move, flag, self.ply)
 
-		return best
+		return bestmove 
 
-	def minimaxVal(game, maxdepth, evaluatorFunction = None):
-		if maxdepth == 0 or game.isLeafNode():
-			if evaluatorFunction:
-				return evaluatorFunction(game)
+	def blocks_allowed(self, old_move, blocks_state):
+    		
+		if old_move[0] == (-1, -1):
+    		return [x for x in xrange(16)]
+		else:
+			r = int(old_move[0] / 4)
+			c = int(old_move[1] / 4)
+    		possblock = r * 4 + c 
+
+			if blocks_state[possblock] != ' ':
+				possblocks = []
+				for i in xrange(16):
+    				if blocks_state[i] == ' ':
+    					possblocks.append(i)
+				return possblocks
+
 			else:
-				return game.score()
+    			return [possblock]
 
-	def alphabetaVal(game, maxdepth, alpha, beta, evaluatorFunction = None):
-		if maxdepth = 0 or game.isLeafNode():
-			if evaluatorFunction:
-				return evaluatorFunction(game)
-			else:
-				return game.score()
+	def cells_allowed(self, tempboard, blocks_allowed, blockstatus):
+    	
+		p_cells = []
+        for block in blocks_allowed:
+                v = (int(block / 4)) * 4
+                h = (block % 4) * 4
+                for i in range(v, v + 4):
+                    for j in range(h, h + 4):
+                        if board[i][j] == '-':
+                            p_cells.append((i, j))
+        return p_cells
 
-		for move in game.find_valid_move_cells():
-			temp = game.copy()
-			temp.play(move)
-
-			if alpha is not None:
-				opponentBeta  = -1 * alpha
-			else:
-				opponentBeta = None
-
-			if beta is not None:
-				opponentAlpha = -1 * beta
-			else:
-				opponentAlpha = None
-
-			val = -1 * alphabetaVal(temp, maxdepth - 1, opponentAlpha, opponentBeta, evaluatorFunction)
-			if (alpha is not None) and (beta is not None) and alpha >= beta:
-				return beta
-
-		return alpha
-
-	def alphabetapruning(game, maxdepth, evaluatorFunction = None):
-
-		bestVal = None
-		bestMove = None
-
-		for move in game.find_valid_move_cells():
-			temp = game.copy()
-			temp.play(move)
-
-			if bestVal is not None:
-				opponentBeta = -1 * bestVal
-			else:
-				opponentBeta = None
-
-			val = -1 * alphabetaVal(temp, maxdepth, None, opponentBeta, evaluatorFunction)
-			if bestVal is None or val > bestVal:
-				(bestVal, bestMove) = (val, move)
-
-		return (bestVal, bestMove)
-
+	def alphabeta():
+	
 
 
 
